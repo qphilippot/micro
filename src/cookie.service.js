b5190481ba5caa0ad54d580606f8d4969ebb33bf
qphilippot <quentin.philippot@gmail.com>
2020-11-21 19:29:14 +0100

    create cookie service

---
+++ src/cookie.service.js
@@ -0,0 +1,45 @@
+const Singleton = require("./singleton.pattern");
+
+
+
+class CookieService {
+    buildCookies(request) {
+        if (
+            typeof request.headers !== 'undefined' &&
+            typeof request.headers.cookie === 'undefined'
+        ) {
+            if (typeof request.__micro === 'undefined') {
+                request.__micro = {};
+            }
+
+            if (typeof request.__micro.cookies === 'undefined') {
+                request.__micro.cookies = {};
+            }
+        }
+
+        else {
+            // https://alligator.io/nodejs/express-cookies/
+            const rawCookies = request.headers.cookie.split('; ');
+            const parsedCookies = {};
+            rawCookies.forEach(rawCookie => {
+                const parsedCookie = rawCookie.split('=');
+                parsedCookies[parsedCookie[0]] = parsedCookie[1];
+            });
+
+            request.microservice_cookies = parsedCookies;
+        }
+    }
+
+    get(request = {}, name = 'a_cookie_name') {
+        if (
+            typeof request.__micro === 'undefined' ||
+            typeof request.__micro.cookies === 'undefined'
+        ) {
+            buildCookies(request);
+        }
+
+        return request.__micro.cookies[name] || null;
+    }
+}
+
+module.exports = Singleton.create(CookieService);

91f1c63a2c947380e9b880cb52704588265fcabd
qphilippot <quentin.philippot@gmail.com>
2020-11-21 20:06:40 +0100

    add abstract and interface usage error

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const RoutingService = require('./src/routing.service');
 const Singleton = require('./src/singleton.pattern');
 const DBHelper = require('./src/DBHelper/db.helper');
 const CookieService = require('./src/cookie.service');
+const AbstractMethodNotImplementedException = require('./src/AbstractMethodNotImplemented.exception');
+const InstantiateAbstractOrInterfaceException = require('./src/InstantiateAbstractOrInterface.exception');
 
 module.exports = {
     BaseController,
@@ -11,5 +13,7 @@ module.exports = {
     RoutingService,
     Singleton,
     CookieService,
-    DBHelper
+    DBHelper,
+    AbstractMethodNotImplementedException,
+    InstantiateAbstractOrInterfaceException
 };

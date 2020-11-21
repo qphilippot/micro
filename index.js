fc10f9851e4984f44681ec7ed444bb5a1643a7a4
qphilippot <quentin.philippot@gmail.com>
2020-11-21 19:38:26 +0100

    create cookie service

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const MicroService = require('./src/microservice');
 const RoutingService = require('./src/routing.service');
 const Singleton = require('./src/singleton.pattern');
 const DBHelper = require('./src/DBHelper/db.helper');
+const CookieService = require('./src/cookie.service');
 
 module.exports = {
     BaseController,
     MicroService,
     RoutingService,
     Singleton,
+    CookieService,
     DBHelper
 };
